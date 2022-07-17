5de7c375f556b4ecc80f0243d7b16bd749ed43ab
Travis Ridgway <ridgwaytravis@gmail.com>
2022-07-17 17:16:27 -0500

    Add single round function

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-let computerSelection = getComputerChoice();
+let playerSelection = prompt("Please choose either ROCK, PAPER, or SCISSORS."); 
+let computerSelection = getComputerChoice()
+let roundWinner = '';
+let playerScore = 0;
+let computerScore = 0;
+
 
 
 function getComputerChoice() {
-    let choices = ['rock', 'paper', 'scissors']
+    let choices = ['ROCK', 'PAPER', 'SCISSORS']
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-console.log(computerSelection);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      roundWinner = 'tie'
+    }
+    if (
+      (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+      (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+      (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+      playerScore++
+      roundWinner = 'player'
+    }
+    if (
+      (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+      (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+      (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    ) {
+      computerScore++
+      roundWinner = 'computer'
+    }
+}   
 
