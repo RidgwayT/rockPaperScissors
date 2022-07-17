ab53360b30f4c1c10ab2fe492c1a06e7981e8112
Travis Ridgway <ridgwaytravis@gmail.com>
2022-07-17 16:52:24 -0500

    Add files and function to generate computer selection

---
+++ script.js
@@ -0,0 +1,10 @@
+let computerSelection = getComputerChoice();
+
+
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+console.log(computerSelection);
+
